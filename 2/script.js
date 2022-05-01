c16bff7c9c1b1860bb5b5a939b42c56080669cc1
Mariano Alves <mariano.dsw@gmail.com>
2022-05-01 12:36:27 -0300

    Mostrando informações na tela e o botão de reset

--- 2/script.js
+++ 2/script.js
@@ -2,34 +2,79 @@
 let matriz = [[0,0,0],[0,0,0],[0,0,0]]
 let moves = 0
 let winner = false
+let filled = []
+
+const MSG = {
+    WINNER: 'Já temos um vencedor!',
+    TIE: 'Deu velha, sem ganhadores!',
+    TAKEN: 'Escolha outro local!'
+}
 
 const player1 = {
-    string: 'X',
-    id: 1
+    id: 1,
+    string: 'X',    
+    class: 'x',
+    score: 0
 }
 const player2 = {
+    id: 2,
     string: 'O',
-    id: 2
+    class: 'o',
+    score: 0
+}
+
+const setUiPlayer = (currentPlayer)=>{
+    document.getElementById('current').innerHTML = currentPlayer.string    
 }
 
 const getPlayer = ()=>{
     let id = Math.floor(Math.random() * (2)) + 1;
+    let p
+
     if(id == 1){
-        return player1
+        p = player1
+    }else{
+        p = player2
     }
-    return player2
+
+    setUiPlayer(p)
+
+    return p
+}
+
+const resetGame = ()=>{
+    matriz = [[0,0,0],[0,0,0],[0,0,0]]
+    winner = false
+    filled = []
+    moves = 0
+
+    let elements = document.getElementsByClassName('row')
+    for (let item of elements) {       
+        item.innerHTML = ''
+        item.classList.remove('winner')
+    }
+
+    player = getPlayer()
+
+    logger(undefined, 'Jogo resetado!')
 }
 
 let player = getPlayer()
 
-let teste = document.getElementsByClassName('row')
-for (let item of teste) { 
+let board = document.getElementById('reset')
+board.addEventListener('click', (e)=>{
+    resetGame()
+})
+
+let elements = document.getElementsByClassName('row')
+for (let item of elements) { 
+
     item.addEventListener('click', (el)=>{
 
         if(winner == true){
-            console.log('Já temos um vencedor!')
+            logger(undefined, MSG.WINNER)
             return
-        }
+        }        
 
         let element = el.target   
     
@@ -40,27 +85,45 @@ for (let item of teste) {
 
             player = changePlayer(player)
         }
+
+        if(moves == 9 && winner == false){
+            logger(undefined, MSG.TIE)
+            return
+        }
     })
     
 }
 
-const changePlayer = (player)=>{
-    if (player.id == 1){
+const changePlayer = (cplayer)=>{
+    
+    if (cplayer.id == 1){
+        setUiPlayer(player2)
         return player2
     }else{
+        setUiPlayer(player1)
         return player1
     }
 }
 
 const setMove = (el, player)=>{
+
+    if(el.id == ""){
+        logger(undefined, MSG.TAKEN)
+        return false
+    }
+
+
     if(matriz[el.id[0]][el.id[1]] == 0){
-        el.innerHTML = player.string
+        
+        el.innerHTML = setElClass(player)
         matriz[el.id[0]][el.id[1]] = player.id
         moves++        
     }else{
-        console.log('Escolha outro local!')
+        logger(undefined, MSG.TAKEN)
         return false
     }
+
+    logger(el)
     return true
 }
 
@@ -73,22 +136,28 @@ const checkRow = ()=>{
     if(moves > 4){
         for(i = 0; i<=2; i++){
             for(j=0; j<=2;j++){                
-                if(matriz[i][0] == matriz[i][j] && matriz[i][j] != 0){                    
+                if(matriz[i][0] == matriz[i][j] && matriz[i][j] != 0 && j>0){
                     match++
-                }   
-                             
+                    if(filled.length <= 2){                        
+                        filled[j] = [i,j].join('')
+                    }                     
+                }           
             }
-            if(match == 3){
-                win = true
+            if(match == 2){
+                win = true                
+                filled[0] = [filled[1][0],0].join('')
+                match = 0
+
+                setUiWinner()                
             }else{
                 match = 0
+                filled = []                
             }
-            
         } 
         if(win){
-            winner = true
-            console.log(player.string +' ganhou!! [r]')
-            alert(player.string +' ganhou [r]')
+            winner = true            
+            logger(undefined, `O jogador ${player.string} Ganhou na linha`)
+            
         }
     }
 }
@@ -102,20 +171,27 @@ const checkCol = ()=>{
     if(moves > 4){
         for(i = 0; i<=2; i++){
             for(j=0; j<=2;j++){                
-                if(matriz[0][i] == matriz[j][i] && matriz[j][i] != 0){
+                if(matriz[0][i] == matriz[j][i] && matriz[j][i] != 0 && j>0){
                     match++
-                }              
-            } 
-            if(match == 3){
-                win = true
+                    if(filled.length <= 2){                        
+                        filled[j] = [j,i].join('')
+                    }     
+                }  
+            }    
+            if(match == 2){
+                win = true                
+                filled[0] = [0,filled[1][1]].join('')
+                match = 0
+                
+                setUiWinner()
             }else{
                 match = 0
+                filled = []   
             }       
         } 
         if(win){
             winner = true
-            console.log(player.string +' ganhou!! [c]')
-            alert(player.string +' ganhou [c]')
+            logger(undefined, `O jogador ${player.string} Ganhou na coluna`)
         }
         
     }
@@ -123,12 +199,56 @@ const checkCol = ()=>{
 
 const checkDiagonal = ()=>{
     if(moves > 4){
-        if((matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]) || (matriz[2][0] == matriz[1][1] && matriz[2][0] == matriz[0][2])){
+        if((matriz[0][0] == matriz[1][1] && matriz[0][0] == matriz[2][2]) && matriz[0][0] != 0 || (matriz[2][0] == matriz[1][1] && matriz[2][0] == matriz[0][2] & matriz[2][0] != 0)){
+            if(matriz[0][0] == matriz[2][2] && matriz[0][0] != 0){
+                filled = ['00','11','22']
+            }else{
+                filled = ['20','11','02']
+            }           
             winner = true
-            console.log(player.string +' Ganhou na diagonal')
-            alert(player.string +' Ganhou [d]')
+            setUiWinner()
+
+            logger(undefined, `${player.string} Ganhou na diagonal`)
         }
     }
 }
+
+const setElClass = (cplayer) =>{    
+    return `<p class="${cplayer.class}">${cplayer.string}</p>`
+}
+
+const setUiWinner = ()=>{
+    for (let item of elements) {
+        for(let f of filled){
+            if(f == item.id){
+                item.classList.add('winner')
+            }
+        }        
+    }
+
+    setScore()
+}
+
+const setScore = ()=>{
+    player.score++   
+    
+    document.getElementById('xscore').innerHTML = player1.score
+    document.getElementById('oscore').innerHTML = player2.score
+
+    logger(undefined, 'mostrando os pontos!')
+}
+
+const logger = (el, msg) =>{
+    if(msg == undefined){
+        console.log(`O jogador ${player.string} jogou na posição ${el.id}`)
+    }else{
+        console.log(`${msg}`)
+    }
+}
+
+
+
+
+
 
 
--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Repositório consiste em pequenos exercícios de javascript
 
 1 - Inverter String
 - Deverá ser informado uma string e a função retornará a string invertida
+
+2 - Jogo da Velha
+- Jogo da velha podendo ser jogado por dois jogadores, na tela mostra a pontuação e a vez do jogador (X ou O). No console mostra todos os passos percorridos.
+
+Conhecimento usados nesse desafio: Manipulação do DOM com javascript puro, css com flexbox.
